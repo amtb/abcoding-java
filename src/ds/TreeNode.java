ded4945adfd57538a67b5f7a624ec30553bd0583
Alpha Barry <alpha.mt.barry@gmail.com>
2021-06-25 22:54:46 +0400

    lc 1110 delete nodes and return forest

--- src/ds/TreeNode.java
+++ src/ds/TreeNode.java
@@ -1,21 +1,38 @@
 package ds;
 
+import java.util.Objects;
+
 // Definition for a binary tree node.
 public class TreeNode {
   public int val;
   public TreeNode left;
   public TreeNode right;
 
-  TreeNode() {
-  }
-
-  TreeNode(int val) {
+  public TreeNode(int val) {
     this.val = val;
   }
 
-  TreeNode(int val, TreeNode left, TreeNode right) {
+  public TreeNode(int val, TreeNode left, TreeNode right) {
     this.val = val;
     this.left = left;
     this.right = right;
+  }
+
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    TreeNode treeNode = (TreeNode) o;
+
+    if (val != treeNode.val) return false;
+    if (!Objects.equals(left, treeNode.left)) return false;
+    return Objects.equals(right, treeNode.right);
+  }
+
+  public int hashCode() {
+    int result = val;
+    result = 31 * result + Objects.hashCode(left);
+    result = 31 * result + Objects.hashCode(right);
+    return result;
   }
 }
